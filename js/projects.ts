136126072657dc6f1b12ccca6750e2fc7fb510ff
henrisama <pedropiresflores@icloud.com>
2022-07-14 21:51:19 -0400

    deploy

---
+++ js/projects.ts
@@ -0,0 +1,86 @@
+export default [
+  {
+    area: "Machine Learning",
+    projects: [
+      {
+        title: "MNIST",
+        overview: "Convolutional neural network for MNIST",
+        technologies: ["Python", "Tensorflow", "Matplotlib", "Sklearn", "CNN"],
+        image_path: "img/MNIST.jpg",
+        link: "https://github.com/henrisama/MNIST",
+      },
+      {
+        title: "Cifar 10",
+        overview: "Convolutional neural network for Cifar 10",
+        technologies: ["Python", "Tensorflow", "Matplotlib", "Sklearn", "CNN"],
+        image_path: "img/cifar10.jpg",
+        link: "https://github.com/henrisama/Cifar10",
+      },
+      {
+        title: "Titanic Survival",
+        overview: "Titanic Surival Competition (Kaggle)",
+        technologies: ["Python", "Seaborn", "Sklearn", "Pandas"],
+        image_path: "img/titanic.jpg",
+        link: "https://github.com/henrisama/Titanic",
+      },
+      {
+        title: "Book Recommendation System",
+        overview: "Book Recommendation System Based Collaborative Filtering.",
+        technologies: ["Python", "Pandas", "Sklearn", "KNN"],
+        image_path: "img/books.jpg",
+        link: "https://github.com/henrisama/Book-Recommendation-System",
+      },
+      {
+        title: "Movie Recommendation System",
+        overview: "Movie Recommendation System Based Collaborative Filtering.",
+        technologies: ["Python", "Pandas", "Sklearn", "KNN"],
+        image_path: "img/movies.jpg",
+        link: "https://github.com/henrisama/Movie-Recommendation-System",
+      },
+    ],
+  },
+  {
+    area: "Games",
+    projects: [
+      {
+        title: "Snake Game",
+        overview: "Simple snake game in Java",
+        technologies: ["Java"],
+        image_path: "img/snake.png",
+        link: "https://github.com/henrisama/Snake",
+      },
+      {
+        title: "Pong Game",
+        overview: "Simple pong game in python",
+        technologies: ["Python"],
+        image_path: "img/pong.png",
+        link: "https://github.com/henrisama/Pong",
+      },
+    ],
+  },
+  {
+    area: "Mobile",
+    projects: [
+      {
+        title: "Buscar Cep",
+        overview: "Simple app to look up address by zip code",
+        technologies: ["Flutter", "Dart"],
+        image_path: "img/busca_cep.png",
+        link: "https://github.com/Mateus-F-Oliveira/flutter_busca_cep",
+      },
+    ],
+  },
+  {
+    area: "AI",
+    projects: [
+      {
+        title: "Puzzle AI",
+        overview:
+          "Using breadth-first search with heuristics for game 15 puzzle",
+        technologies: ["Python", "BFS"],
+        image_path: "img/puzzle15.jpg",
+        link: "https://github.com/henrisama/Puzzle-AI",
+      },
+    ],
+  },
+];
